b58c2fc3656191a79ca6be45f26be3498e2e47ab
Abdelrahman <abdelrahmanah30@email.com>
2026-03-23 08:55:42 +0300

    to_learner_python

---
+++ python/ft_garden_intro.py
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_intro.py                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kali <marvin@42.fr>                        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/23 07:46:11 by kali              #+#    #+#              #
+#    Updated: 2026/03/23 08:51:20 by kali             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+if __name__ == "__main__":
+	name = "plant: Rose"
+	height = "Hieght: 25cm"
+	age = "Age: 30days"
+	print("=== Welcome to My Garden ===")
+	print(name)
+	print(height)
+	print(age)
+	print("=== End of Program ===")
